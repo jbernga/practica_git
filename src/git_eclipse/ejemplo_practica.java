4bb979ee1e5068c6ab79506cf52a9ef4e074557f
jbernga <joseantonio89albatera@gmail.com>
2023-02-13 19:49:55 +0100

    no añado nada

---
+++ src/git_eclipse/ejemplo_practica.java
@@ -0,0 +1,16 @@
+package git_eclipse;
+
+public class ejemplo_practica {
+
+	public ejemplo_practica() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
